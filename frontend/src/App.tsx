31b6e081f1d98b1c1a7d3e966061d3920435efbb
Saranga siriwardhana <lasindusaranga99@gmail.com>
2024-05-08 11:18:23 +0530

    Footter Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,16 +6,25 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import HeaderPrimary from './components/Header/HeaderPrimary';
-
+import Footer from './components/Footer/Footer';
+import { ChakraProvider } from '@chakra-ui/react'
 export default function App() {
   return (
+    <ChakraProvider>
     <BrowserRouter>
     <HeaderPrimary />
+    <div className='py-10' >
     <Routes>
+      
         <Route path="/" element={<Home />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+      
     </Routes>
+    </div>
+    <Footer/>
+    
     </BrowserRouter>
+    </ChakraProvider>
   );
 }
