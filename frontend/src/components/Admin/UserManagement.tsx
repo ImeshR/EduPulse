8e755b86596072c49cdd7da7acc97d3a8f5888a4
Saranga siriwardhana <lasindusaranga99@gmail.com>
2024-05-10 01:06:16 +0530

    create user management and course management UI

--- frontend/src/components/Admin/UserManagement.tsx
+++ frontend/src/components/Admin/UserManagement.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
-
-export default function UserManagement() {
-  console.log('UserManagement.tsx')
+import React from 'react';
+import { FaPlusCircle, FaEye } from 'react-icons/fa';
+import userManagement from '/22.png'
+const UserManagement = () => {
   return (
-    <div>
-        <div className="text-center bg-red-700 z-50">
-            <h1 className="display-4">User Management</h1>
-            <p className="lead">View and manage users</p>
+    <div className="container font-mono mx-auto mt-2">
+      <div className="text-center  z-50">
+        <h1 className="text-4xl font-bold text-blue-500 underline">User Management</h1>
+        
+        <p className="text-lg text-blue-300">View and manage users</p>
+        <img src={userManagement} alt="User Management" className=" mt-4 w-96 h-28 mx-auto" />
+      </div>
+
+      <div className="flex justify-center mt-8 mt-4font-mono space-x-6">
+        {/* Create New Course Card */}
+        <div className="w-96 h-52 shadow-2xl bg-blue-50 rounded-lg  p-6 flex flex-col items-center justify-center transform transition-transform hover:scale-105">
+          <FaPlusCircle className="text-4xl text-blue-500 mb-4" />
+          <h2 className="text-xl font-bold">Create New Users</h2>
+          <p className="text-lg text-gray-600 mt-2">Add a new user to the platform</p>
         </div>
-      
+
+        {/* View My Courses Card */}
+        <div className="w-96 h-52 shadow-2xl bg-blue-50 rounded-lg  p-6 flex flex-col items-center justify-center transform transition-transform hover:scale-105">
+          <FaEye className="text-4xl text-green-500 mb-4" />
+          <h2 className="text-xl font-bold">All Users</h2>
+          <p className="text-lg text-gray-600 mt-2">Browse and manage  existing users</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default UserManagement;
