712b59f72c1fe47cd6de6c349333313bb8690b2c
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2024-05-09 08:04:37 +0530

    updated

--- course-management/controllers/courseManagementController.js
+++ course-management/controllers/courseManagementController.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createCourse } from '../services/courseService.js';
+import { createCourse, updateCourse , getAllCourses , getCourseById } from '../services/courseService.js';
 
 const router = express.Router();
 
@@ -8,6 +8,38 @@ router.post("/create", async (req, res) => {
   try {
     const { name, description, summary, createdBy, price, duration, courseContent } = req.body;
     const course = await createCourse(name, description, summary, createdBy, price, duration, courseContent);
+    res.json(course);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//update a course by id
+router.put("/update/:id", async (req, res) => {
+  try {
+    const { name, description, summary, createdBy, price, duration, courseContent } = req.body;
+    const course = await updateCourse(req.params.id, name, description, summary, createdBy, price, duration, courseContent);
+    res.json(course);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+//get all courses
+router.get("/getAll", async (req, res) => {
+  try {
+    const courses = await  getAllCourses();
+    res.json(courses);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+//get course by id
+router.get("/:id", async (req, res) => {
+  try {
+    const course = await getCourseById(req.params.id);
     res.json(course);
   } catch (error) {
     res.status(500).json({ error: error.message });
