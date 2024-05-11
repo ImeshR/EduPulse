7c0afcb0e0b834f72537e07156429b9121f1001f
Saranga siriwardhana <lasindusaranga99@gmail.com>
2024-05-11 16:06:38 +0530

    add components to StudentDashboard

---
+++ frontend/src/pages/StudentDashboard/StudentDashboard.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Slider from '../../components/StudentDashboard/Slider'
+import MyCourses from '../../components/StudentDashboard/MyCourses'
+import LatestCourses from '../../components/StudentDashboard/LatestCourses'
+import { Box, Text } from "@chakra-ui/react";
+import LernerOutcomes from '../../components/StudentDashboard/LernerOutcomes';
+
+export default function StudentDashboard() {
+  return (
+    <div>
+      <div className=' min-h-screen bg-[#ffffff] items-center'>
+
+        <Slider />
+        <MyCourses />
+        <LatestCourses />
+        <Box
+          border="4px solid"
+          borderColor="gray.300"
+          borderRadius="md"
+          padding="4"
+          textAlign="center"
+          width="95%"
+          margin="auto"
+          bgColor="blue.500"
+        >
+          <Text
+            style={{
+              fontStyle: "bold",
+              fontFamily: "Heading Font Name",
+              fontWeight: "900",
+              fontSize: "30px",
+              color: "white",
+            }}
+          >
+            " Education is the most powerful weapon which you can use to change
+            the world."
+          </Text>
+        </Box>
+        <LernerOutcomes/>
+
+
+
+      </div>
+
+    </div>
+  )
+}
