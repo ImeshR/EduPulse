9d6b4b4d0f89ed0c4fa9e8116bc69e9af06327ef
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2024-05-16 15:22:29 +0530

    updated

--- frontend/src/components/Payment/UpdatePaymentData.tsx
+++ frontend/src/components/Payment/UpdatePaymentData.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
-import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements, Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Button, FormControl, FormLabel } from "@chakra-ui/react";
 
-const UpdatePaymentData = ({ userId, authToken, onClose }) => {
+
+const publishableKey = 'pk_test_51PEpR3P8dDb7QY7tKrBzy4J2YxgLPMx8sa4GPNpGeuuI3ESshjyW1qXZAx3VHioialpcMMZuTE1kHTL4f5gAuYuf00MRP8Okqi';
+const stripePromise = loadStripe(publishableKey);
+
+
+const UpdatePaymentDataForm = ({ userId, authToken, onClose }) => {
   const stripe = useStripe();
   const elements = useElements();
   const [error, setError] = useState(null);
@@ -10,14 +17,11 @@ const UpdatePaymentData = ({ userId, authToken, onClose }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setLoading(true);
-
     if (!stripe || !elements) {
       return;
     }
-
     const cardElement = elements.getElement(CardElement);
     const { token, error } = await stripe.createToken(cardElement);
-
     if (error) {
       setError(error.message);
       setLoading(false);
@@ -40,27 +44,101 @@ const UpdatePaymentData = ({ userId, authToken, onClose }) => {
           }
         );
 
-        const data = await response.json();
-        console.log(data);
-
+        const responseData = await response.json(); // Parse response JSON
+        if (responseData.message === 'Card details updated successfully') {
+          alert('Card details updated successfully');
+          onClose();
+        } else {
+          setError('Failed to save card');
+        }
         setLoading(false);
-        onClose(); // Close the modal on successful update
       } catch (error) {
         console.error(error);
-        setError("Failed to update card details");
+        setError('Failed to save card');
         setLoading(false);
       }
+
     }
   };
 
+
   return (
     <form onSubmit={handleSubmit}>
-      <CardElement />
-      <button type="submit" disabled={!stripe || loading}>
-        {loading ? "Processing..." : "Update Card"}
-      </button>
-      {error && <div style={{ color: "red" }}>{error}</div>}
+      <FormControl>
+        <FormLabel>Card Details</FormLabel>
+        <CardElement />
+      </FormControl>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      <Button
+        mt={4}
+        colorScheme="blue"
+        isLoading={loading}
+        type="submit"
+        disabled={!stripe}
+      >
+        update card
+      </Button>
     </form>
+  );
+}
+
+const UpdatePaymentData = ({ userId, authToken, onClose }) => {
+
+  // const stripe = useStripe();
+  // const elements = useElements();
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   setLoading(true);
+
+  //   if (!stripe || !elements) {
+  //     return;
+  //   }
+
+  //   const cardElement = elements.getElement(CardElement);
+  //   const { token, error } = await stripe.createToken(cardElement);
+
+  //   if (error) {
+  //     setError(error.message);
+  //     setLoading(false);
+  //   } else {
+  //     try {
+  //       const response = await fetch(
+  //         `http://localhost:7072/api/paymentMangement/update-card`,
+  //         {
+  //           method: "POST",
+  //           headers: {
+  //             "Content-Type": "application/json",
+  //             Authorization: `Bearer ${authToken}`,
+  //           },
+  //           body: JSON.stringify({
+  //             userId,
+  //             updatedCardDetails: {
+  //               token: token.id,
+  //             },
+  //           }),
+  //         }
+  //       );
+
+  //       const data = await response.json();
+  //       console.log(data);
+
+  //       setLoading(false);
+  //       onClose(); // Close the modal on successful update
+  //     } catch (error) {
+  //       console.error(error);
+  //       setError("Failed to update card details");
+  //       setLoading(false);
+  //     }
+  //   }
+  // };
+
+  return (
+    <Elements stripe={stripePromise}>
+      <UpdatePaymentDataForm userId={userId} authToken={authToken} onClose={onClose} />
+    </Elements>
   );
 };
 
